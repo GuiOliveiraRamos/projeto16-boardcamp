7fd0740c070137aaf960db86dca0f524f8b0f059
GuiOliveiraRamos <guioliveiraramos30@gmail.com>
2023-07-27 13:39:10 -0300

    feat: update customer

--- src/controllers/clientes.controllers.js
+++ src/controllers/clientes.controllers.js
@@ -47,3 +47,29 @@ export async function getCustomersById(req, res) {
     res.status(500).send(err.message);
   }
 }
+
+export async function updateCustomer(req, res) {
+  const { id } = req.params;
+  const { name, phone, cpf, birthday } = req.body;
+  try {
+    const customer = await db.query(`SELECT * FROM customers WHERE id = $1;`, [
+      id,
+    ]);
+    if (customer.rowCount === 0) return res.sendStatus(404);
+    const validation = schemaCustomers.validate({ name, phone, cpf, birthday });
+    if (validation.error) return res.sendStatus(400);
+    const alreadyExists = await db.query(
+      `SELECT * FROM customers WHERE cpf = $1 AND id <> $2;`,
+      [cpf, id]
+    );
+    if (alreadyExists.rowCount > 0) return res.sendStatus(409);
+
+    await db.query(
+      `UPDATE customers SET name = $1, phone = $2, cpf = $3, birthday = $4 WHERE id = $5;`,
+      [name, phone, cpf, birthday, id]
+    );
+    res.sendStatus(200);
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+}
--- src/routes/clientes.routes.js
+++ src/routes/clientes.routes.js
@@ -3,6 +3,7 @@ import {
   getCustomers,
   getCustomersById,
   newCustomer,
+  updateCustomer,
 } from "../controllers/clientes.controllers.js";
 
 const customersRouter = Router();
@@ -10,6 +11,6 @@ const customersRouter = Router();
 customersRouter.post("/customers", newCustomer);
 customersRouter.get("/customers", getCustomers);
 customersRouter.get("/customers/:id", getCustomersById);
-customersRouter.put("/customers/:id");
+customersRouter.put("/customers/:id", updateCustomer);
 
 export default customersRouter;
